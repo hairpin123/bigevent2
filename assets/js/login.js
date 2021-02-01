2e700f502ccf44dab72c407d2e0d1d1ef0d69f8c
hairpin123 <1359860296@qq.com>
2021-02-01 20:24:48 +0800

    完成登录和注册功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,25 +4,22 @@ $(function () {
 		$('.login-box').hide();
 		$('.reg-box').show();
 	});
-
 	// 点击“去登录”的链接
 	$('#link_login').on('click', function () {
 		$('.login-box').show();
 		$('.reg-box').hide();
 	});
 
-	// 验证密码框
 	var form = layui.form;
 	var layer = layui.layer;
-
 	form.verify({
-		// 自定义一个pwd校验规则
+		// 自定义pwd的校验规则
 		pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-		// 校验两次密码是否一致
+		// 校验两次密码是否一致的规则
 		repwd: function (value) {
 			var pwd = $('.reg-box [name=password]').val();
 			if (pwd !== value) {
-				return '两次密码不一致';
+				return '两次密码不一致！';
 			}
 		},
 	});
@@ -34,13 +31,13 @@ $(function () {
 			method: 'POST',
 			url: '/api/reguser',
 			data: $(this).serialize(),
-			suceess: function (res) {
+			success: function (res) {
 				if (res.status !== 0) {
 					return layer.msg(res.message);
 				}
-				layer.msg('注册成功，请登录！', { time: 800 }, function () {
-					$('#link_login').click();
-				});
+				layer.msg('注册成功，请登录！');
+				// 模拟人的点击行为
+				$('#link_login').click();
 			},
 		});
 	});
@@ -57,8 +54,10 @@ $(function () {
 					return layer.msg('登录失败！');
 				}
 				layer.msg('登录成功！', { time: 800 }, function () {
+					// 将登录得到的token字符串，保存到localStorage中
 					localStorage.setItem('token', res.token);
-					location.href = 'index.html';
+					// 跳转到后台主页
+					location.href = '/index.html';
 				});
 			},
 		});
